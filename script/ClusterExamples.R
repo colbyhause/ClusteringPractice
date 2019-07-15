59253555b35e16b7d0882b9b5c8df1a2e5a45274
colbyhause <chause@ucdavis.edu>
2019-07-15 11:43:05 -0700

    worked through tutorial sent from christine: this one has soe useful functions in it

--- script/ClusterExamples.R
+++ script/ClusterExamples.R
@@ -1,4 +1,4 @@
-# clustering practice/ tutorial from this site: wasnt super helpful...
+# clustering practice/ tutorial from this site: wasnt super helpful----
 # https://www.r-bloggers.com/k-means-clustering-in-r/
 
 
@@ -21,6 +21,8 @@ ggplot(iris, aes(Petal.Length, Petal.Width, color = iris$cluster)) + geom_point(
 
 
 mydata
+
+#another practice set: ----
 
 # practive from this site:
 # https://www.guru99.com/r-k-means-clustering.html
@@ -126,3 +128,144 @@ ggplot(data = center_reshape, aes(x = features, y = cluster, fill = values)) +
   scale_fill_gradientn(colours = hm.palette(90)) +
   theme_classic()
 
+
+# Cluster tutorial sent from Christine:----
+# from site: https://uc-r.github.io/kmeans_clustering#kmeans
+
+library(tidyverse)  # data manipulation
+library(cluster)    # clustering algorithms
+#install.packages("factoextra")
+library(factoextra) # clustering algorithms & visualization
+
+# dataframe to work with:
+df <- USArrests
+
+# remove missing data
+df <- na.omit(df)
+which(is.na(df))
+
+# scale the data to standardize it:
+df <- scale(df)
+head(df)
+
+# calculate dissimilarity by getting distances:
+distance <- get_dist(df) # calculate distances
+fviz_dist(distance, gradient = list(low = "#00AFBB", mid = "white", high = "#FC4E07")) # visualize by creating a dustance matrix
+
+# K-means clustering:
+# K-means algorithm can be summarized as follows:
+#1. Specify the number of clusters (K) to be created (by the analyst)
+#2. Select randomly k objects from the data set as the initial cluster centers or means
+#3. Assigns each observation to their closest centroid, based on the Euclidean distance between the object and the centroid
+#4. For each of the k clusters update the cluster centroid by calculating the new mean values of all the data points in the cluster. The centroid of a Kth cluster is a vector of length p containing the means of all variables for the observations in the kth cluster; p is the number of variables.
+#5.Iteratively minimize the total within sum of square (Eq. 7). That is, iterate steps 3 and 4 until the cluster assignments stop changing or the maximum number of iterations is reached. By default, the R software uses 10 as the default value for the maximum number of iterations.
+
+k2 <- kmeans(df, centers = 2, nstart = 25) # nstart means the function will attempt that many initial configurations and report on the best one 
+str(k2)
+
+# View clustering results with fviz_cluster--> provides visualization:
+# fviz_cluster will perform principal component analysis (PCA) and plot the data points according to the first two principal components that explain the majority of the variance.
+
+fviz_cluster(k2, data = df)
+
+#you can also use pair-wise scatter plot to see the clustering pattern:
+df %>%
+  as_tibble() %>%
+  mutate(cluster = k2$cluster,
+         state = row.names(USArrests)) %>%
+  ggplot(aes(UrbanPop, Murder, color = factor(cluster), label = state)) +
+  geom_text()
+
+
+# better to try many different grouping (k) options, not just set on 2:
+k3 <- kmeans(df, centers = 3, nstart = 25)
+k4 <- kmeans(df, centers = 4, nstart = 25)
+k5 <- kmeans(df, centers = 5, nstart = 25)
+
+# plots to compare
+p1 <- fviz_cluster(k2, geom = "point", data = df) + ggtitle("k = 2")
+p2 <- fviz_cluster(k3, geom = "point",  data = df) + ggtitle("k = 3")
+p3 <- fviz_cluster(k4, geom = "point",  data = df) + ggtitle("k = 4")
+p4 <- fviz_cluster(k5, geom = "point",  data = df) + ggtitle("k = 5")
+
+library(gridExtra)
+grid.arrange(p1, p2, p3, p4, nrow = 2)
+
+# Method above ^ does not provide information on the ideal number of k groups,use one of these methods:
+# Elbow method
+# Silhouette method
+# Gap statistic
+
+#1. Elbow method: want to determine how many clusters are needed so that the total within-cluster sum of square (wss) (which measures the compactness of the clustering) is as small as possible and does not get much smaller with more groups
+# do this by calculating the wss over a range of group options (ie. 1-10) and then plot those values. Where the curve bends is the appropriate number of groups 
+
+wss <- function(k) {
+  kmeans(df, k, nstart = 10 )$tot.withinss
+}
+
+k.values <- 1:15
+
+# extract wss for 2-15 clusters
+wss_values <- map_dbl(k.values, wss)
+
+plot(k.values, wss_values,
+     type="b", pch = 19, frame = FALSE, 
+     xlab="Number of clusters K",
+     ylab="Total within-clusters sum of squares")
+
+#OR better yet use this single function for elbow method:
+set.seed(123)
+fviz_nbclust(df, kmeans, method = "wss")
+
+#2. Silhouette method:
+#This method measure the quality of the clustering. So a high average silhouette width indicates good clustering, so when plotting this method the group that is maximized is the optimal group number for clustering:
+
+# the long way:
+## function to compute average silhouette for k clusters
+avg_sil <- function(k) {
+  km.res <- kmeans(df, centers = k, nstart = 25)
+  ss <- silhouette(km.res$cluster, dist(df))
+  mean(ss[, 3])
+}
+
+# Compute and plot wss for k = 2 to k = 15
+k.values <- 2:15
+
+# extract avg silhouette for 2-15 clusters
+avg_sil_values <- map_dbl(k.values, avg_sil)
+
+plot(k.values, avg_sil_values,
+     type = "b", pch = 19, frame = FALSE, 
+     xlab = "Number of clusters K",
+     ylab = "Average Silhouettes")
+
+# wrapped up into one function:
+fviz_nbclust(df, kmeans, method = "silhouette") # so says 2 is the optimal number of groups, but 4 groups is a close second option
+
+#3. Gap statistic: this approach can be applied to ANY clustering method (kmeans, hierarchical, etc.). The gap statistic compares the total intracluster variation with different values of K with their expected values under null reference distribution of the data (i.e. a distribution with no obvious clustering).
+#SO: That is, for each variable  in the data set we compute its range and generate values for the n points uniformly from the interval min to max.
+
+# compute gap statistic
+set.seed(123)
+gap_stat <- clusGap(df, FUN = kmeans, nstart = 25,
+                    K.max = 10, B = 50)
+print(gap_stat, method = "firstmax")
+
+#We can visualize the results with fviz_gap_stat which suggests four clusters as the optimal number of clusters.
+
+fviz_gap_stat(gap_stat) # shows that 4 clusters are the best option
+
+# overall, the results of the 3 methods mostly point towards 4 clusters, so use that many in the analysis:
+
+set.seed(123)
+final <- kmeans(df, 4, nstart = 25)
+print(final)
+
+#visualize:
+fviz_cluster(final, data = df)
+
+# do some descriptive statistics based onthe clustering:
+USArrests %>%
+  mutate(Cluster = final$cluster) %>%
+  group_by(Cluster) %>%
+  summarise_all("mean")
